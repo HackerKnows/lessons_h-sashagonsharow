d9bd61c86ca057810e58929fdd7b48377767ec8f
Anzerus <Anzerus@mail.ru>
2018-03-28 20:14:08 +0300

    описание!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ 03. ifs/ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите координаты x");
+            double coordinatesX = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите координаты y");
+            double coordinatesY = double.Parse(Console.ReadLine());
+            if (coordinatesX < 4)
+            {
+                Console.WriteLine("координата лежит на стороне 1");
+            }
+            else
+            {
+                Console.WriteLine("координата лежит на стороне 2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03. ifs/вычесление роста и веса/вычесление роста и веса/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вычесление_роста_и_веса
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите рост!");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите весс!");
+            double weight = double.Parse(Console.ReadLine());
+            if (height - 100 < weight)
+            {
+                Console.WriteLine("вам нужно похудеть");
+            }
+            else { 
+                Console.WriteLine("вам нужно поправится");
+            }   
+                Console.ReadLine();
+        }
+    }
+}
