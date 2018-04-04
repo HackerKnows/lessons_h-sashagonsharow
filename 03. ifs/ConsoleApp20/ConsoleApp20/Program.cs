290b3ceaac342b5e6f58242f3a2d573bdcf661d1
Anzerus <Anzerus@mail.ru>
2018-04-04 20:18:06 +0300

    Я НЕ СДЕЛАЛ ДЗ

---
+++ 03. ifs/ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите сторону a");
+            double sideA = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите сторону b");
+            double sideB = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите сторону c");
+            double sideС = double.Parse(Console.ReadLine());
+            if (sideA + sideB>sideС)
+            {
+                Console.WriteLine("треугольник существует");
+            }
+            else
+            {
+                Console.WriteLine("треугольник не существует");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03. ifs/ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите имя первого игрока");
+            string nameOfFirstPlayer = Console.ReadLine();
+            Console.WriteLine("введите имя второго игрока");
+            string nameOfSecondPlayer = Console.ReadLine();
+            Console.WriteLine("введите рейтинг первого игрока");
+            int ratingOfFirstPlayer =int.Parse(Console.ReadLine());
+            Console.WriteLine("введите рейтинг второго игрока");
+            int ratingOfSecondPlayer =int.Parse(Console.ReadLine());
+            if(ratingOfFirstPlayer> ratingOfSecondPlayer)
+            {
+                Console.WriteLine(nameOfSecondPlayer +"                              "+nameOfFirstPlayer); 
+            }
+            else
+            {
+                Console.WriteLine(nameOfFirstPlayer + "                              " + nameOfSecondPlayer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
